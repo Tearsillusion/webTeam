76f729b42b0f8b4547a544381a4d09ab548c946b
lvjunling <1585345738@qq.com>
2022-04-24 03:50:18 +0800

    fix:修改Dockerfile

--- web.sh
+++ web.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-WORK_PATH = '/webTest/webTeam/web'
-cd $WORK_PATH
+
+
 git reset --hard origin/webManage
 git clean -f
 git pull origin webManage
 docker build -t web-images ./docker
-docker build --progress=plain -t node-images -f $WORK_PATH/docker/Dockerfile-node.buildkit $WORK_PATH
-docker build --progress=plain -t web-images -f $WORK_PATH/docker/Dockerfile-web.buildkit $WORK_PATH
+docker build --progress=plain -t node-images -f ./docker/Dockerfile-node.buildkit .
+docker build --progress=plain -t web-images -f ./docker/Dockerfile-web.buildkit .
 docker stop web-container
 docker rm web-container
 docker run -p 9001:80 --name web-container -d web-images
