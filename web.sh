974a1d9180f4385330ffd8b41fc5998f39e09904
lvjunling <1585345738@qq.com>
2022-04-24 04:37:00 +0800

    fix:修改Dockerfile

--- web.sh
+++ web.sh
@@ -8,7 +8,7 @@ git pull origin webManage
 docker stop web-container
 docker rm web-container
 docker build --progress=plain -t web-team-pre -f ./web/docker/Dockerfile-pre.buildkit ./web
-docker build --progress=plain -t web-container:latest -f ./web/docker/Dockerfile.buildkit ./web
+docker build --progress=plain -t web-images:latest -f ./web/docker/Dockerfile.buildkit ./web
 docker run -itd -p 9001:80 --name web-container  web-images
 
 
