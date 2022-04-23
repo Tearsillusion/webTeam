61b78fa02b8a5df44114d17b5907af2437494d7a
lvjunling <1585345738@qq.com>
2022-04-24 02:24:59 +0800

    fix:修改manage.sh

--- manage.sh
+++ manage.sh
@@ -3,7 +3,7 @@
 git reset --hard origin/webManage
 git clean -f
 git pull origin webManage
-docker build -t manage-images ./manage/docker
+docker build -t manage-images ./manage
 docker stop manage-container
 docker rm manage-container
 docker container run -p 9008:3000 --name manage-container -d manage-images
