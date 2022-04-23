11e129aa23dae16a70f3ac0ca8307911c5166770
lvjunling <1585345738@qq.com>
2022-04-24 01:37:20 +0800

    fix:修改sh文件

--- manage.sh
+++ manage.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-WORK_PATH='/webTest/webTeam'
-cd $WORK_PATH
 git reset --hard origin/webManage
 git clean -f
 git pull origin webManage
 docker build -t manage-images ./manage/docker
 docker stop manage-container
 docker rm manage-container
-docker container run -p 3000:3000 --name manage-container -d manage-images
+docker container run -p 9008:3000 --name manage-container -d manage-images
 
