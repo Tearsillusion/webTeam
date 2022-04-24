3bfa06d9095edd6f381bfa2703886e65353a6aec
lvjunling <1585345738@qq.com>
2022-04-24 19:03:10 +0800

    提交

--- web.sh
+++ web.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 
-
+WORK_PATH='./web'
 git reset --hard origin/webManage
 git clean -f
 git pull origin webManage
 docker stop web-container
 docker rm web-container
+cd $WORK_PATH
 make build-image
 docker run -itd -p 9002:80 --name web-container  web-team:latest
 
