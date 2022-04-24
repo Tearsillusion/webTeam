2a49b04a5dea0dfa66dcc7cab99446af4eca0cab
lvjunling <1585345738@qq.com>
2022-04-24 11:02:10 +0800

    feat:增加mysql镜像

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 
-
+docker stop mysql-images
+docker rm mysql-images
 docker build -t mysql-images ./manage/docker
 docker run -itd --name mysql-container -p 3306:3306 -e MYSQL_ROOT_PASSWORD=ljl1234 mysql-images
 
