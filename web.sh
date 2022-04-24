485dc8ad9232ea9e076fc35ac99ae7536bf35f5b
lvjunling <1585345738@qq.com>
2022-04-24 17:39:43 +0800

    提交

--- web.sh
+++ web.sh
@@ -8,8 +8,8 @@ git pull origin webManage
 docker stop web-container
 docker rm web-container
 docker build --progress=plain -t web-team-pre -f ./web/docker/Dockerfile-pre.buildkit ./web
-docker build --progress=plain -t web-images:latest -f ./web/docker/Dockerfile.buildkit ./web
-docker run -itd -p 9002:80 --name web-container  web-images:latest
+docker build --progress=plain -t web-team:latest -f ./web/docker/Dockerfile.buildkit ./web
+docker run -itd -p 9002:80 --name web-container  web-team:latest
 
 
 
