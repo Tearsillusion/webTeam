a3402a74b60c587e31d4ddffd0a10ba98b23da62
lvjunling <1585345738@qq.com>
2022-04-24 10:50:42 +0800

    feat:增加mysql镜像

--- manage.sh
+++ manage.sh
@@ -4,7 +4,9 @@ git reset --hard origin/webManage
 git clean -f
 git pull origin webManage
 docker build -t manage-images ./manage
+docker build -t mysql-images ./manage/docker
 docker stop manage-container
 docker rm manage-container
 docker run -itd -p 9008:3000 --name manage-container  manage-images
+docker run -itd --name mysql-container -p 3306:3306 -e MYSQL_ROOT_PASSWORD=123456 mysql-images
 
